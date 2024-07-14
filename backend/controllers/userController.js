7cc79cb80045f09cd5422eb15710cae4f2bbe391
marcbeep <77645923+marcbeep@users.noreply.github.com>
2024-07-14 12:13:31 +0100

    feat(signup): example

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from '../models/userModel.js';
+import Job from '../models/jobModel.js';
 import jwt from 'jsonwebtoken';
 import UserAnalytics from '../models/userAnalyticsModel.js';
+import AppStats from '../models/appStatsModel.js';
 
 const createToken = (_id) =>
   jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' });
@@ -27,6 +29,27 @@ export const signupUser = async (req, res) => {
     });
 
     await userData.save();
+
+    // Create an example job for the new user
+    const exampleJob = new Job({
+      userId: user._id,
+      title: 'Example!',
+      description:
+        "This is an example job to show you how things work around here. You can start tracking your job applications by hitting the new job button. It's best to copy and paste all the info for a job here to keep everything in one place. If a job has no set application deadline, just select a date in the future you'd like to finish your application!",
+      status: 'incomplete',
+      company: 'Acme Inc',
+      jobType: 'gradscheme',
+      dateApplied: new Date(),
+    });
+
+    await exampleJob.save();
+
+    // Update application stats in AppStats
+    const appStats = await AppStats.findOne();
+    if (appStats) {
+      appStats.noApplications += 1;
+      await appStats.save();
+    }
 
     const token = createToken(user._id);
     res.status(201).json({ email: user.email, token });
